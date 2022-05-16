77235be9bce9fd2562db706b15e67c8e375ae4e6
lesliechen <435421355@qq.com>
2022-05-17 01:34:12 +0800

    feat:增加开发配置项

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import vue from "@vitejs/plugin-vue";
 import path from "path";
 //@ts-ignore
 import viteCompression from "vite-plugin-compression";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { AntDesignVueResolver } from "unplugin-vue-components/resolvers";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,6 +19,42 @@ export default defineConfig({
       threshold: 10240,
       algorithm: "gzip",
       ext: ".gz",
+    }),
+    AutoImport({
+      // targets to transform
+      include: [/\.[tj]sx?$/, /\.vue$/, /\.vue\?vue/, /\.md$/],
+
+      // global imports to register
+      imports: [
+        // 插件预设支持导入的api
+        "vue",
+        "vue-router",
+        "pinia",
+        // 自定义导入的api
+      ],
+
+      // Generate corresponding .eslintrc-auto-import.json file.
+      // eslint globals Docs - https://eslint.org/docs/user-guide/configuring/language-options#specifying-globals
+      eslintrc: {
+        enabled: false, // Default `false`
+        filepath: "./.eslintrc-auto-import.json", // Default `./.eslintrc-auto-import.json`
+        globalsPropValue: true, // Default `true`, (true | false | 'readonly' | 'readable' | 'writable' | 'writeable')
+      },
+
+      // Filepath to generate corresponding .d.ts file.
+      // Defaults to './auto-imports.d.ts' when `typescript` is installed locally.
+      // Set `false` to disable.
+      dts: "./auto-imports.d.ts",
+    }),
+    // 自动引入组件
+    Components({
+      resolvers: [
+        AntDesignVueResolver({
+          // 参数配置可参考：https://github.com/antfu/unplugin-vue-components/blob/main/src/core/resolvers/antdv.ts
+          // 自动引入 ant-design/icons-vue中的图标，需要安装@ant-design/icons-vue
+          resolveIcons: true,
+        }),
+      ],
     }),
   ],
   // 配置别名
@@ -40,13 +79,22 @@ export default defineConfig({
     proxy: {},
   },
   // 生产环境打包配置
-  //去除 console debugger
   build: {
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
+    target: "es2015",
+    cssTarget: "chrome80",
+    // minify: 'terser',
+    /**
+     * 当 minify=“minify:'terser'” 解开注释
+     * Uncomment when minify="minify:'terser'"
+     */
+    // terserOptions: {
+    //   compress: {
+    //     keep_infinity: true,
+    //     drop_console: VITE_DROP_CONSOLE,
+    //   },
+    // },
+    // Turning off brotliSize display can slightly reduce packaging time
+    brotliSize: false,
+    chunkSizeWarningLimit: 2000,
   },
 });
